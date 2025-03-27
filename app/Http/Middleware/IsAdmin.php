c468ed0e3cc47187dff1f251f317661bbf161c49
Diwas Sigdel <diwassigdel18@gmail.com>
2025-03-27 23:30:01 +0545

    IsAdmin corrected

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class isadmin
+class IsAdmin
 {
     /**
      * Handle an incoming request.
